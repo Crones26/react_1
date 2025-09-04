326511dc44e546b08aa18b3bead30b07a18f34d7
Cympak <Cympak@DESKTOP-J0NFRBK>
2025-09-04 17:22:58 +0500

    Introduction

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import logo from '../../logo.svg'   //Scalable Vector Graphics
+
+function Header() {
+    let title = "Introduction to React"
+    return(
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>{title}</h1>
+            <h2>JSON - JavaScript Object Notation</h2>
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
+    )
+}
+
+export default Header;
