474ad21f9aef3c7b84222a4dd6991f706ed57104
Cympak <Cympak@DESKTOP-J0NFRBK>
2025-09-05 11:13:18 +0500

    Class DONE

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from '../logo.svg';
 import './App.css';
 import Header from './header/Header.js';
-import Footer from './footer/Footer.js'; 
+import Footer from './footer/Footer.js';
 import Article from './article/Article.js';
-import Nav from './nav/Nav.js'; 
+import Nav from './nav/Nav.js';
 import data from './db.json';
 
-function App(props) {
-  let db = data.people;
+export default function App(props) {
+  const db = data.people;
 
   return (
-      <div className='app'>
-        <Nav navigation={props.navigation}/>
-        <Header/>
-        <Article db = {db}/>
-      </div>
+    <div className="App">
+      <Nav navigation={props.navigation} />
+      <Header />
+      <Article db={db} />
+      <Footer />
+    </div>
   );
 }
-
-export default App;
